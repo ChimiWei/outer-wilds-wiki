43b7c74bdbb02582e999b3370214d16420295936
Wei Oliveira <wei.cesaroliveira@gmail.com>
2022-09-08 18:37:55 -0300

    rotation speed fix (fix)

--- src/components/planetImg/PlanetImg.jsx
+++ src/components/planetImg/PlanetImg.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PlanetIMG = styled.img`
     max-width: ${props => props.size}vw;
     animation: rotate 333s linear infinite;
-    -webkit-animation: rotate 100s linear infinite;
+    -webkit-animation: rotate 333s linear infinite;
   @keyframes rotate {
     from {
      -webkit-transform: rotate(0deg);
